cd5ff6fbc84f2c9f84c5925acce423e3c3495575
Daniel Sim <daniel.ssq89@gmail.com>
2020-04-08 00:23:09 +0800

    wait why?

--- app.js
+++ app.js
@@ -6,43 +6,6 @@ function escapeDNValue(s) {
   )
 }
 
-function make_csr() {
-  // 3) with generateKeypair
-  const kp = KEYUTIL.generateKeypair("RSA", 2048);
-
-  document.getElementById("private-key").textContent = KEYUTIL.getPEM(kp.prvKeyObj, "PKCS1PRV")
-  document.getElementById("public-key").textContent = KEYUTIL.getPEM(kp.pubKeyObj)
-
-  const subjectString = [
-    ['C', document.getElementById("country").value],
-    ['ST', document.getElementById("province").value],
-    ['L', document.getElementById("city").value],
-    ['O', document.getElementById("organization").value],
-    ['OU', document.getElementById("ou").value],
-    ['CN', document.getElementById("cn").value],
-  ]
-  .concat(
-    document.getElementById('domain').value.split('.').map(dc => ['DC', dc])
-  )
-  .filter(([k, v]) => v.trim())
-  .map(([k, v]) => `${k}=${escapeDNValue(v.trim())}`)
-  .join(',');
-
-  console.log(subjectString)
-
-  const pem = KJUR.asn1.csr.CSRUtil.newCSRPEM({
-    subject: {str: KJUR.asn1.x509.X500Name.ldapToOneline(subjectString)},
-    sbjpubkey: kp.pubKeyObj,
-    sigalg: "SHA256withRSA",
-    sbjprvkey: kp.prvKeyObj
-  });
-  document.getElementById("certificate-signing-request").textContent = pem
-}
-
-function download_private_key() {
-  download(document.getElementById("private-key").value, "client.key", "application/octet-stream")
-}
-
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: '#app',
